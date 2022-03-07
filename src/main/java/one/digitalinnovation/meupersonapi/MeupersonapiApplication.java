4dbc497a8efacc15605e0289a12767ddd339cc82
João Carlos Vernetti dos Santos <jcvernetti@gmail.com>
2022-03-07 18:30:58 -0300

    first commit

---
+++ src/main/java/one/digitalinnovation/meupersonapi/MeupersonapiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.meupersonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeupersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeupersonapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/meupersonapi/MeupersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.meupersonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeupersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
