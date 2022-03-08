eb7d8cdc1a0f120541f958812c67df335e4e0985
João Carlos Vernetti dos Santos <jcvernetti@gmail.com>
2022-03-08 17:05:54 -0300

    Created PersonMapper

---
+++ src/main/java/one/digitalinnovation/meupersonapi/mapper/PersonMapper.java
@@ -0,0 +1,18 @@
+package one.digitalinnovation.meupersonapi.mapper;
+
+import one.digitalinnovation.meupersonapi.dto.request.PersonDTO;
+import one.digitalinnovation.meupersonapi.entity.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface PersonMapper {
+
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO personDTO);
+
+    PersonDTO toDTO(Person person);
+}
